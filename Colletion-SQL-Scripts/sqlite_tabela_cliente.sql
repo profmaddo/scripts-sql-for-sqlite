ae38f69fa8293c33e1b5cbc1cd9f28e2ef325f31
Professor Marco Maddo <profmaddo@gmail.com>
2021-04-18 22:04:10 -0400

    Script SQL Create Table | SQLite Syntax
    Script SQL Create Table | SQLite Syntax

---
+++ Colletion-SQL-Scripts/sqlite_tabela_cliente.sql
@@ -0,0 +1,11 @@
+CREATE TABLE cliente 
+(id INTEGER PRIMARY KEY AUTOINCREMENT,
+ 	nome TEXT,
+ 	salario REAL, 
+ 	preco NUMERIC,
+ 	idade INTEGER,
+ 	ativo INTEGER,
+ 	dataCadastro TEXT,
+ 	horaCadastro TEXT
+ )
+
